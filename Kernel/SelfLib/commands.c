437a5b64888134afa6a7d6344311fdaba740a97d
Patrick <patrickmdey@hotmail.com>
2020-05-21 18:19:56 -0300

    help

--- Kernel/SelfLib/commands.c
+++ Kernel/SelfLib/commands.c
@@ -4,7 +4,13 @@
 //uint8_t **commands = {"help", "inforeg", "printmem", "time", "cpuid", "temp", ""};
 
 void help(){
-    printStringLn("Help: prints all available commands and ");
+    printStringLn("Help: prints all available commands");
+    printStringLn("Inforeg: prints the value of all the registers on screen");
+    printStringLn("Printmem: recieves a pointer and makes a memory dump of 32 bytes on screen");
+    printStringLn("Time: prints the current system time on screen");
+    printStringLn("Cpuid: prints the processor brand and model on screen");
+    printStringLn("Temp: prints the current processor temperature on screen");
+
 }
 
 //todo: print registers, done in assembly
@@ -19,7 +25,7 @@ void printmem(uint8_t * mem) {
 
 //todo: desplays system time
 void time() {
-
+    
 }
 
 //todo: shows processor model and brand
