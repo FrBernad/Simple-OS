c518c39806bde9e477cf11b188d9c9901538a48d
Nicolas Rampoldi <nrampoldi@itba.edu.ar>
2020-05-24 01:28:00 -0300

    added functions in assembly

--- Userland/SampleCodeModule/lib.asm
+++ Userland/SampleCodeModule/lib.asm
@@ -6,14 +6,82 @@ GLOBAL
 GLOBAL
 GLOBAL
 GLOBAL
-GLOBAL
-GLOBAL
-GLOBAL
-GLOBAL
-GLOBAL
-GLOBAL
+GLOBAL help
+GLOBAL inforeg
+GLOBAL 
+GLOBAL time
+GLOBAL cpuInfo
+GLOBAL temp
 
 section .text
 
+help:
+    push rbp
+    mov rbp, rsp
 
+    push rax
+
+    mov rax, 9
+    int 80h
+
+    pop rax
+
+    leave
+    ret
+
+inforeg:
+    push rbp
+    mov rbp, rsp
+
+    push rax
+
+    mov rax, 10
+    int 80h
+
+    pop rax
+
+    leave
+    ret
+
+time:
+    push rbp
+    mov rbp, rsp
+
+    push rax
+
+    mov rax, 12
+    int 80h
+
+    pop rax
+
+    leave
+    ret
+
+cpuInfo:
+    push rbp
+    mov rbp, rsp
+
+    push rax
+
+    mov rax, 13
+    int 80h
+
+    pop rax
+
+    leave
+    ret
+
+temp:
+    push rbp
+    mov rbp, rsp
+
+    push rax
+
+    mov rax, 14
+    int 80h
+
+    pop rax
+
+    leave
+    ret
 
