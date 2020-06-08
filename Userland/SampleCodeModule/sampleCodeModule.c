4aa6e0c9a2778031216244522bff5ac819920ddd
Francisco <frbernad@itba.edu.ar>
2020-06-08 22:33:22 +0000

    final changes

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,9 +1,4 @@
 #include <taskManager.h>
-#include <staticStack.h>
-#include <buffer.h>
-#include <stringLib.h>
-#include <staticQueue.h>
-#include <systemCalls.h>
 
 int main() {
       initApps();
